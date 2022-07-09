6674a31bb46b7bdad4a9917d35f586d1c8e11e9e
user42 <nfauconn@student.42.fr>
2022-07-09 13:30:57 +0200

    memmove OK

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 17:53:30 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/07/08 18:38:46 by user42           ###   ########.fr       */
+/*   Updated: 2022/07/09 13:15:34 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*dest_addr;
-	unsigned char	*source_addr;
-	size_t	i;
+	char	*dest;
+	char	*source;
 
 	if (!dst && !src)
 		return (NULL);
-	if (dst == src || !len)
-		return (dst);
-	dest_addr = dst;
-	source_addr = (void *)src;
-	if (source_addr > dest_addr && source_addr - dest_addr < (int)len)
+	dest = (char *)dst;
+	source = (char *)src;
+	if (dest < source)
 		while (len--)
-			*dest_addr++ = *source_addr++;
-	else if (dest_addr < source_addr && dest_addr - source_addr < (int)len)
-		while (len--)
-			dest_addr[len] = source_addr[len];
+			*dest++ = *source++;
 	else
-		memcpy(dst, src, len);
+	{
+		dest += len - 1;
+		source += len - 1;
+		while (len--)
+			*dest-- = *source--;
+	}
 	return (dst);
 }
--- testMEMMOVE.c
+++ testMEMMOVE.c
@@ -4,7 +4,21 @@
 
 int	main()
 {
-	printf("\n", av[1], );
-	printf("\n", av[1], );
+	char src[100] = "COUCOOOOU";
+
+	ft_memmove(src+5, src, strlen(src)+1);
+	printf("ft_memmove = %s\n", src);
+
+	char src1[100] = "COUCOOOOU";
+
+	memmove(src1+5, src1, strlen(src1)+1);
+	printf("   memmove = %s\n", src1);
+	return (0);
+
+	ft_memmove(NULL, NULL, 3);
+	printf("ft_memmove = %s\n", src);
+
+	memmove(NULL, NULL, 3);
+	printf("   memmove = %s\n", src1);
 	return (0);
 }
